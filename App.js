fde99aeb3b1cd47980033de129bdcce7d8aec3b8
Justina Kuchta <justyna.kuchta@gmail.com>
2020-10-12 10:36:08 -0400

    added permissions and incoming notification handling

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
+
+// define how incoming notofications are handled if the app is running
+// this will let the system know what to do with an incoming notification
+Notifications.setNotificationHandler({
+  handleNotification: () => {
+    return {}
+  }
+})
 
 export default function App() {
+
+  useEffect(() => {
+    // this will do nothing on Android
+    Permissions.getAsync(Permissions.NOTIFICATIONS).then(statusObj => {
+      if (statusObj.status !== 'granted') {
+        return Permissions.askAsync(Permissions.NOTIFICATIONS)
+      } else {
+        return statusObj
+      }
+    }).then(statusObj => {
+      if (statusObj.status !== 'granted') {
+        Alert.alert(
+          'notifications denied', 
+          'we will not be able to send you notifications', 
+          [
+            {text: 'ok', onPress: () => console.log('pressed')}
+          ]
+        )
+      }
+    })
+  }, [])
 
   const triggerNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({
